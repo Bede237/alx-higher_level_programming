72c2cedc489c3697725f1f2d458db8a416c20342
Bede237 <byounkwe@gmail.com>
2023-09-04 15:38:47 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if there is aa cycle in a linked list
+ * @list: linked list
+ * Return: 1 (True) 0 (False)
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+	listint_t *check;
+	int i, j;
+
+	i = 0;
+	current = list;
+	while (current != NULL)
+	{
+		check = list;
+		j = 0;
+		while (j != i)
+		{
+			if (current == check)
+				return (1);
+			else
+				check = check->next;
+			j++;
+		}
+		current = current->next;
+		i++;
+	}
+	return (0);
+}
