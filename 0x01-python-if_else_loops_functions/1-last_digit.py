2768f3bb77bd81c6f82e18ce3d2c45d43b0631b6
Bede237 <byounkwe@gmail.com>
2023-09-05 12:54:45 -0700

    1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+plum = number % 10
+if (number < 0):
+    plum = (number * -1) % 10
+    plum = plum * -1
+print(f"Last digit of {number} is {plum}", end = " ")
+if ((plum < 6) and (plum != 0)):
+    print("and is less than 6 and not 0")
+elif (plum > 5):
+    print("and is greater than 5")
+else:
+    print("and is 0")
