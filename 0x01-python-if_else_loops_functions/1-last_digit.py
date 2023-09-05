a13dd57567915617741fbb0f88fbda834dd63016
Bede237 <byounkwe@gmail.com>
2023-09-05 12:57:11 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ plum = number % 10
 if (number < 0):
     plum = (number * -1) % 10
     plum = plum * -1
-print(f"Last digit of {number} is {plum}", end = " ")
+print(f"Last digit of {number} is {plum}", end=" ")
 if ((plum < 6) and (plum != 0)):
     print("and is less than 6 and not 0")
 elif (plum > 5):
