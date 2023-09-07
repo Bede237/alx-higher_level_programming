d0abd8b015d1b19a8bcb87f4671ac012ace2bfa8
Bede237 <byounkwe@gmail.com>
2023-09-07 07:10:43 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def main():
     import sys
+    i = 1
     n = len(sys.argv)
     print((n - 1), "argument:")
     if (n - 1) > 0:
         for i in range(n):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+            print("{}: {}".format(i, sys.argv[i]))
 
 
 if __name__ == "__main__":
