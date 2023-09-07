0ccc742729fa8bb9fa976fd0c526c2dc6f17ade9
Bede237 <byounkwe@gmail.com>
2023-09-07 06:43:48 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def main():
-    import calculator_1
+    import calculator_1 as math
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, math.add(a, b)))
+    print("{} - {} = {}".format(a, b, math.sub(a, b)))
+    print("{} * {} = {}".format(a, b, math.mul(a, b)))
+    print("{} / {} = {}".format(a, b, math.div(a, b)))
 
 
 if __name__ == "__main__":
