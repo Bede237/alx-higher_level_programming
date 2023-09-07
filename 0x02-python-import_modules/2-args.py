a3eefef267f37b6f9835031b84a09d0dc0ef5309
Bede237 <byounkwe@gmail.com>
2023-09-07 07:20:09 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,16 @@
 def main():
     import sys
     i = 1
-    n = len(sys.argv)
-    print((n - 1), "argument:")
-    if (n - 1) > 0:
-        for i in range(n):
+    n = len(sys.argv) - 1
+    print((n - 1), "argument", end='')
+    if n == 1:
+        print(":")
+    elif n > 1:
+        print("s:")
+    else:
+        print("s.")
+    if (n) > 0:
+        for i in range(n + 1):
             print("{}: {}".format(i, sys.argv[i]))
 
 
