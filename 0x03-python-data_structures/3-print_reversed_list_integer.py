85019eeb51c77f92eebc1ef39dd77b7a05a85853
Bede237 <byounkwe@gmail.com>
2023-09-11 13:46:11 -0700

    minor errors

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     spam = len(my_list) - 1
     for i in range(len(my_list)):
-        print("{}".format(my_list[spam - i]))
+        print("{:d}".format(my_list[spam - i]))
