2afead37a871e3b4acd296c1eb66e2aee9bfb224
Bede237 <byounkwe@gmail.com>
2023-09-14 12:11:50 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
+    mat = []
+    for row in matrix:
+        mat.append(list(map(lambda x: x**2, row)))
+    return mat
