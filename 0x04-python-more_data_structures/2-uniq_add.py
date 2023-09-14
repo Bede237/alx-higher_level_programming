2956a6fefd9c80642ac8add5a2c5d71e33edec05
Bede237 <byounkwe@gmail.com>
2023-09-14 13:10:46 -0700

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if not my_list:
+        return None
+    new = []
+    count = 0
+    for i in my_list:
+        if i not in new:
+            count +=  i
+            new.append(i)
+    return count
